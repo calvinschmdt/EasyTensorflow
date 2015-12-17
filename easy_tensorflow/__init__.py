9c0ab8ae389e58f88f5b59c663b0134896943569
Calvin Schmidt <calvinschmdt@gmail.com>
2015-12-17 10:25:31 -0800

    First commit

---
+++ easy_tensorflow/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Provides objects that allow for easy setup, training, and running of neural networks, based on Google's tensorflow library. Also allows for the evolution of effective networks using a genetic algorithm derived from the DEAP package."""
+
+from easy_tensorflow import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ easy_tensorflow/metadata.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'easy_tensorflow'
+project = "EasyTensorflow"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Provides objects that allow for easy setup, training, and running of neural networks, based on Google\'s ' \
+              'tensorflow library. Also allows for the evolution of effective networks using a genetic algorithm derived' \
+              ' from the DEAP package.'
+authors = ['Calvin Schmidt']
+authors_string = ', '.join(authors)
+emails = ['calvins@stanford.edu']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'https://github.com/calvinschmdt/EasyTensorflow'
